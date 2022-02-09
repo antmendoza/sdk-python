24581d49cf551335a92e50e2cb496b22d67812e9
Antonio Mendoza Pérez <antmendoza@gmail.com>
2022-02-09 13:27:14 +0100

    wip

--- tests/serverlessworkflow/sdk/test_action.py
+++ tests/serverlessworkflow/sdk/test_action.py
@@ -6,10 +6,7 @@ from serverlessworkflow.sdk.function_ref import FunctionRef
 
 class TestAction(unittest.TestCase):
 
-
     def test_load_function_ref(self):
-
         action = Action(functionRef=FunctionRef(refName="refNameTest"))
         self.assertTrue(isinstance(action.functionRef, FunctionRef))
         self.assertTrue(action.functionRef.refName, "refNameTest")
-
